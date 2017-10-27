d5b18cc5b602094f788a87dd6bd7eba41199baa8
Petr Kozorezov <petr.kozorezov@gmail.com>
2017-10-27 16:30:02 +0300

    wip: add handling for undefined logger

--- src/raft_logger.erl
+++ src/raft_logger.erl
@@ -4,7 +4,7 @@
 -export_type([event /0]).
 -export([log/4]).
 
--type logger() :: raft_utils:mod_opts().
+-type logger() :: raft_utils:mod_opts() | undefined.
 -type event() ::
       timeout
     | {incoming_message, raft_rpc:endpoint(), raft_rpc:message()}.
@@ -14,6 +14,8 @@
 
 -spec log(logger(), event(), Before::raft:state(), After::raft:state()) ->
     ok.
+log(undefined, _, _, _) ->
+    ok;
 log(Logger, Event, StateBefore, StateAfter) ->
     _ = raft_utils:apply_mod_opts(Logger, log, [Event, StateBefore, StateAfter]),
     ok.
